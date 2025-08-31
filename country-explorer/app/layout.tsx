60780a05e95deb22df7daafda9be01ef52266407
vish-han <vshlchauhab@gmail.com>
2025-08-31 15:48:20 +0530

    Metadata added

--- country-explorer/app/layout.tsx
+++ country-explorer/app/layout.tsx
@@ -1,26 +1,22 @@
 import './globals.css';
 import { Inter, Playfair_Display,Comfortaa } from 'next/font/google';
+import {Metadata} from "next";
 
- const inter = Inter({
-    subsets: ['latin'],
-    variable: '--font-inter',
-    display: 'swap',
-});
  const comfortaa = Comfortaa({
      subsets: ['latin'],
      variable: '--font-comfortaa',
      display: 'swap',
  })
 
- const playfair = Playfair_Display({
-    subsets: ['latin'],
-    variable: '--font-playfair',
-    display: 'swap',
-});
+export const metadata: Metadata = {
+    title: "Country Explorer",
+    description: "This is the Website for explorling every country that is present on the earth",
+    metadataBase: new URL("https://country-explorer-assesment.vercel.app/"),
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en" className={`${inter.variable} ${playfair.variable} ${comfortaa.variable}`}>
+        <html lang="en" className={`${comfortaa.variable}`}>
         <body className="font-sans antialiased">{children}</body>
         </html>
     );
