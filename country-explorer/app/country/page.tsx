0454a8d63b5c4d235b9ca113382ecc889f260bf4
vish-han <vshlchauhab@gmail.com>
2025-08-31 15:40:58 +0530

    file-structure-fix

--- country-explorer/app/country/page.tsx
+++ country-explorer/app/country/page.tsx
@@ -75,6 +75,7 @@ export default function CountrySearchPage() {
                 {/* Countries Grid */}
                 <div className="max-w-7xl mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+
                         {countries.map((country, index) => (
                             <CountryCard
                                 key={`${country.cca3}-${index}`}
